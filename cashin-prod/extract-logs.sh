035c5f7666cfe477531362f5e1c7aba661e2577b
renzo.ferro <renzo.ferro@sesitdigital.com>
2024-11-21 13:53:41 -0500

    .

--- cashin-prod/extract-logs.sh
+++ cashin-prod/extract-logs.sh
@@ -2,6 +2,6 @@ cd /var/opt/extract-logs-dry-run/
 
 logfile="/var/opt/extract-logs-dry-run/cashin-prod/logs/logs_extract_$(date +%Y%m%d).log"
 
-source ./var/opt/extract-logs-dry-run/env/bin/activate
+source /var/opt/extract-logs-dry-run/env/bin/activate
 
 /usr/bin/python3.9 /var/opt/extract-logs-dry-run/cashin-prod/extract-logs.py >> "$logfile" 2>&1
